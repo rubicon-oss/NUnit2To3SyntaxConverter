4ec5a4ede10ef5e498a18ee7ccee88c66290c23a
Daniel Kudernatsch <daniel.kudernatsch@rubicon.eu>
2020-02-18 15:31:12 +0100

    Implementation of renaming of changed assert names

---
+++ Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace NUnit2To3SyntaxConverter.Extensions
+{
+  public static class DictionaryExtensions
+  {
+    public static TValue GetValueOrDefault<TKey, TValue> (this IReadOnlyDictionary<TKey, TValue> _this, TKey key, TValue defaultValue = default)
+    {
+      return _this.TryGetValue (key, out var value)
+          ? value
+          : defaultValue;
+    }
+  }
+}
