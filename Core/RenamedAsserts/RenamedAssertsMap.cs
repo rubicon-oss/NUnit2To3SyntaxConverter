0a02d9a891eb3ff2be6d21bb4e483d1f932d63ce
Daniel Kudernatsch <daniel.kudernatsch@rubicon.eu>
2020-02-28 10:19:30 +0100

    changed Assert document converted to be composition root made unittest more clear added license headers

--- Core/RenamedAsserts/RenamedAssertsMap.cs
+++ Core/RenamedAsserts/RenamedAssertsMap.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿#region copyright
+
+// 
+// Copyright (c) rubicon IT GmbH
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,7 +26,7 @@ using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace NUnit2To3SyntaxConverter.RenamedAsserts
 {
-  public class RenamedAsserts
+  public class RenamedAssertsMap
   {
     private IReadOnlyDictionary<string, IReadOnlyDictionary<string, string>> NewNameMap
       => new Dictionary<string, IReadOnlyDictionary<string, string>>
@@ -55,9 +72,9 @@ namespace NUnit2To3SyntaxConverter.RenamedAsserts
     private (ExpressionSyntax, SimpleNameSyntax) BuildNewSyntax (string newName, ExpressionSyntax expression, SimpleNameSyntax access)
     {
       var nameParts = newName.Split ('.');
-      
-      Trace.Assert(nameParts.Length > 1);
-      
+
+      Trace.Assert (nameParts.Length > 1);
+
       if (nameParts.Length == 2)
       {
         return (IdentifierName (nameParts[0]), access.WithIdentifier (Identifier (nameParts[1])));
