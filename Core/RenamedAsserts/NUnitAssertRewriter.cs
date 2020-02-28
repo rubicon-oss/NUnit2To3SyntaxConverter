ee5f1df716779927e13feb7bd221da73dead1a1e
Daniel Kudernatsch <daniel.kudernatsch@rubicon.eu>
2020-02-28 10:19:30 +0100

    changed Assert document converted to be composition root made unittest more clear added license headers

--- Core/RenamedAsserts/NUnitAssertRewriter.cs
+++ Core/RenamedAsserts/NUnitAssertRewriter.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿#region copyright
+
+// 
+// Copyright (c) rubicon IT GmbH
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -7,15 +24,22 @@ namespace NUnit2To3SyntaxConverter.RenamedAsserts
 {
   public class NUnitAssertRewriter : CSharpSyntaxRewriter
   {
-    private readonly RenamedAsserts _renamedAsserts = new RenamedAsserts();
-    private readonly NUnitAssertRenamer _assertRenamer = new NUnitAssertRenamer();
+    private readonly RenamedAssertsMap _renamedAssertsMap;
+    private readonly NUnitAssertRenamer _assertRenamer;
+
+    public NUnitAssertRewriter (RenamedAssertsMap renamedAssertsMap, NUnitAssertRenamer assertRenamer, bool visitIntoStructuredTrivia = false)
+        : base (visitIntoStructuredTrivia)
+    {
+      _renamedAssertsMap = renamedAssertsMap;
+      _assertRenamer = assertRenamer;
+    }
 
     public override SyntaxNode VisitMemberAccessExpression (MemberAccessExpressionSyntax node)
     {
-      var maybeNewName = _renamedAsserts.TryGetSyntax (node.Expression, node.Name);
-      
-      return maybeNewName.HasValue 
-          ? _assertRenamer.Transform(node, maybeNewName.Value) 
+      var maybeNewName = _renamedAssertsMap.TryGetSyntax (node.Expression, node.Name);
+
+      return maybeNewName.HasValue
+          ? _assertRenamer.Transform (node, maybeNewName.Value)
           : node;
     }
   }
--- UnitTests/RenamedAsserts/NUnitAssertRenamingTest.cs
+++ UnitTests/RenamedAsserts/NUnitAssertRenamingTest.cs
@@ -1,4 +1,21 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿#region copyright
+
+// 
+// Copyright (c) rubicon IT GmbH
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using Microsoft.CodeAnalysis.CSharp;
 using NUnit.Framework;
 using NUnit2To3SyntaxConverter.RenamedAsserts;
 
@@ -7,50 +24,59 @@ namespace NUnit2To3SyntaxConverter.UnitTests.RenamedAsserts
   public class NUnitAssertRenamingTest
   {
     [Test]
-    [TestCase ("Text.DoesNotMatch ($1)", "Does.Not.Match ($1)")]
-    [TestCase ("Text.Matches ($1)", "Does.Match ($1)")]
-    [TestCase ("Text.DoesNotEndWith ($1)", "Does.Not.EndWith ($1)")]
-    [TestCase ("Text.EndsWith ($1)", "Does.EndWith ($1)")]
-    [TestCase ("Text.DoesNotStartWith ($1)", "Does.Not.StartWith ($1)")]
-    [TestCase ("Text.StartsWith ($1)", "Does.StartWith ($1)")]
-    [TestCase ("Text.Contains ($1)", "Does.Contain ($1)")]
-    [TestCase ("Text.DoesNotContain ($1)", "Does.Not.Contain ($1)")]
-    [TestCase ("Is.InstanceOfType<T> ($1)", "Is.InstanceOf<T> ($1)")]
-    [TestCase ("Is.StringStarting ($1)", "Does.StartWith ($1)")]
-    [TestCase ("Is.StringContaining ($1)", "Does.Contain ($1)")]
-    [TestCase ("Is.StringEnding ($1)", "Does.EndWith ($1)")]
-    [TestCase ("Is.StringMatching ($1)", "Does.Match ($1)")]
+    [TestCase ("Text.DoesNotMatch (...)", "Does.Not.Match (...)")]
+    [TestCase ("Text.Matches (...)", "Does.Match (...)")]
+    [TestCase ("Text.DoesNotEndWith (...)", "Does.Not.EndWith (...)")]
+    [TestCase ("Text.EndsWith (...)", "Does.EndWith (...)")]
+    [TestCase ("Text.DoesNotStartWith (...)", "Does.Not.StartWith (...)")]
+    [TestCase ("Text.StartsWith (...)", "Does.StartWith (...)")]
+    [TestCase ("Text.Contains (...)", "Does.Contain (...)")]
+    [TestCase ("Text.DoesNotContain (...)", "Does.Not.Contain (...)")]
+    [TestCase ("Is.InstanceOfType<T> (...)", "Is.InstanceOf<T> (...)")]
+    [TestCase ("Is.StringStarting (...)", "Does.StartWith (...)")]
+    [TestCase ("Is.StringContaining (...)", "Does.Contain (...)")]
+    [TestCase ("Is.StringEnding (...)", "Does.EndWith (...)")]
+    [TestCase ("Is.StringMatching (...)", "Does.Match (...)")]
     public void TestAllSimpleCases (string toRename, string expected)
     {
       var expression = SyntaxFactory.ParseExpression (toRename);
-      var actual = new NUnitAssertRewriter().Visit (expression);
+      var rewriter = new NUnitAssertRewriter (new RenamedAssertsMap(), new NUnitAssertRenamer());
+
+      var actual = rewriter.Visit (expression);
+
       Assert.That (actual.ToFullString(), Is.EqualTo (expected));
     }
 
     [Test]
-    [TestCase ("Text\n.DoesNotMatch ($1)", "Does.Not\n.Match ($1)")]
-    [TestCase ("Text.\nMatches ($1)", "Does.\nMatch ($1)")]
-    [TestCase ("Text. DoesNotEndWith ($1)", "Does.Not. EndWith ($1)")]
-    [TestCase ("Text .EndsWith ($1)", "Does .EndWith ($1)")]
-    [TestCase ("Text.\n        DoesNotStartWith ($1)", "Does.Not.\n        StartWith ($1)")]
-    [TestCase ("Text\n        .StartsWith ($1)", "Does\n        .StartWith ($1)")]
-    [TestCase ("Text\n        .StartsWith($1)", "Does\n        .StartWith($1)")]
+    [TestCase ("Text\n.DoesNotMatch (...)", "Does.Not\n.Match (...)")]
+    [TestCase ("Text.\nMatches (...)", "Does.\nMatch (...)")]
+    [TestCase ("Text. DoesNotEndWith (...)", "Does.Not. EndWith (...)")]
+    [TestCase ("Text .EndsWith (...)", "Does .EndWith (...)")]
+    [TestCase ("Text.\n        DoesNotStartWith (...)", "Does.Not.\n        StartWith (...)")]
+    [TestCase ("Text\n        .StartsWith (...)", "Does\n        .StartWith (...)")]
+    [TestCase ("Text\n        .StartsWith(...)", "Does\n        .StartWith(...)")]
     public void DoesHandleWhitespace (string toRename, string expected)
     {
       var expression = SyntaxFactory.ParseExpression (toRename);
-      var actual = new NUnitAssertRewriter().Visit (expression);
+      var rewriter = new NUnitAssertRewriter (new RenamedAssertsMap(), new NUnitAssertRenamer());
+
+      var actual = rewriter.Visit (expression);
+
       Assert.That (actual.ToFullString(), Is.EqualTo (expected));
     }
 
     [Test]
     [TestCase ("")]
     [TestCase ("Text")]
-    [TestCase ("Text.Other ($1)")]
-    [TestCase ("Is.Other ($1)")]
+    [TestCase ("Text.Other (...)")]
+    [TestCase ("Is.Other (...)")]
     public void DoesNotRewriteNonChangedAsserts (string toRename)
     {
       var expression = SyntaxFactory.ParseExpression (toRename);
-      var actual = new NUnitAssertRewriter().Visit (expression);
+      var rewriter = new NUnitAssertRewriter (new RenamedAssertsMap(), new NUnitAssertRenamer());
+
+      var actual = rewriter.Visit (expression);
+
       Assert.That (actual, Is.EqualTo (expression));
     }
   }
