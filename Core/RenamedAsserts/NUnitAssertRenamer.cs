680b59a3f5fddc3c7bcb032f8723e3424e2c8a09
Daniel Kudernatsch <daniel.kudernatsch@rubicon.eu>
2020-02-28 10:21:38 +0100

    formatting

--- Core/RenamedAsserts/NUnitAssertRenamer.cs
+++ Core/RenamedAsserts/NUnitAssertRenamer.cs
@@ -20,16 +20,15 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace NUnit2To3SyntaxConverter.RenamedAsserts
 {
-  public class NUnitAssertRenamer: ISyntaxTransformer<MemberAccessExpressionSyntax, (ExpressionSyntax, SimpleNameSyntax)>
+  public class NUnitAssertRenamer : ISyntaxTransformer<MemberAccessExpressionSyntax, (ExpressionSyntax, SimpleNameSyntax)>
   {
     public MemberAccessExpressionSyntax Transform (MemberAccessExpressionSyntax node, (ExpressionSyntax, SimpleNameSyntax) context)
     {
-
       var (newExpression, newAccessor) = context;
 
       var exprWithTrivia = newExpression.WithTriviaFrom (node.Expression);
       var accessorWithTrivia = newAccessor.WithTriviaFrom (node.Name);
-      
+
       return node.WithExpression (exprWithTrivia)
           .WithName (accessorWithTrivia);
     }
