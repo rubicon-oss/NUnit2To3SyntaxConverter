6e31f9c2e3ae36f51aa5b4be4e1181bbd65244d7
Daniel Kudernatsch <daniel.kudernatsch@rubicon.eu>
2020-03-10 10:40:11 +0100

    fixed licenseheaders

--- Core/RenamedAsserts/AssertRenamer.cs
+++ Core/RenamedAsserts/AssertRenamer.cs
@@ -1,7 +1,4 @@
-﻿#region copyright
-
-// 
-// Copyright (c) rubicon IT GmbH
+﻿// Copyright (c) rubicon IT GmbH
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -12,8 +9,7 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 // FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-#endregion
+//
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
